62282f67882fce6be65de7840fcf7879f9f3e35a
Shubham Barore <shubham07barore@gmail.com>
2022-05-25 00:13:39 +0530

    completed Adding and fetching data from JSON server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+  const [enteredValue, setEnteredValue] = useState("");
+
+  const pushNewValue = () => {
+    fetch("http://localhost:8080/todos", {
+      method: "POST",
+      body: JSON.stringify({
+        value: enteredValue,
+        isCompleted: false
+      }),
+      headers: { "Content-type": "application/json" }
+    }).then(res => res.json())
+      .then(data => {
+        setTodos([...todos, data])
+      });
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:8080/todos/")
+      .then((res) => {
+        return res.json();
+      }).then((data) => {
+        setTodos(data);
+        setEnteredValue("");
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Using JSON server and UseEffect</h1>
+      <input onChange={(e) => setEnteredValue(e.target.value)} />
+      <button
+        onClick={pushNewValue}
+      >ADD</button>
+      {
+        todos.map(x => (
+          <h1 key={x.id}>{x.value}</h1>
+        ))
+      }
     </div>
   );
 }
